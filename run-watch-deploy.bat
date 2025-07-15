ab866e92b5c29cedc3def0784252254aced82847
Rajesh <rajeshrajatv@gmail.com>
2025-07-15 17:24:22 +0530

    fist

--- run-watch-deploy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Test run at %DATE% %TIME% >> "C:\Users\rajes\bat-debug.txt"
-"C:\Program Files\Git\bin\bash.exe" -c "echo running bash >> /c/Users/rajes/deploy-log.txt; /c/Users/rajes/Desktop/Project/spring-boot-microservice-GitHubActions/watch-and-deploy.sh >> /c/Users/rajes/deploy-log.txt 2>&1"
-echo Exit code: %ERRORLEVEL% >> "C:\Users\rajes\bat-debug.txt"
